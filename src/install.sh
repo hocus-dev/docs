a1b3e8870ff9a96d39622fc47c3221f37291fbe8
Hugo Dutka <contact@hugodutka.com>
2020-10-26 18:52:05 +0100

    add a working install.sh script

--- src/install.sh
+++ src/install.sh
@@ -1,1 +1,40 @@
-echo "Hello, world!"
+install() {
+    set -e
+
+    UNSUPPORTED_OS_MSG="
+We don't seem to provide prebuilt binaries for this OS.
+To install hocus, try building it from source.
+
+Check out https://hocus.dev/installation.html for more information.
+"
+    AFTER_INSTALLATION_MSG="
+Hocus has been installed.
+To verify the installation was successful, please restart your shell and run
+
+$ hocus
+
+If it was not, please ensure that $HOME/bin is in PATH and that $HOME/bin/hocus exists.
+"
+
+    case "$(uname -a)" in
+        *"Darwin"* ) FILENAME="hocus-macos-x86_64";;
+        *"GNU/Linux"* ) FILENAME="hocus-linux-x86_64-gnu";;
+        * ) echo "$UNSUPPORTED_OS_MSG" && return 1;;
+    esac
+
+    echo "Downloading hocus..."
+
+    mkdir -p "$HOME/bin"
+    DOWNLOAD_LINK="https://github.com/hocus-dev/cli/releases/latest/download/$FILENAME"
+    OUTPUT_LOCATION="$HOME/bin/hocus"
+    curl -sSL "$DOWNLOAD_LINK" --output "$OUTPUT_LOCATION"
+    chmod +x "$OUTPUT_LOCATION"
+
+    if [ ":$PATH:" != *":$HOME/bin:"* ]; then
+        echo 'export PATH="$PATH:$HOME/bin"' >> "$HOME/.profile"
+    fi
+
+    echo "$AFTER_INSTALLATION_MSG"
+}
+
+install
