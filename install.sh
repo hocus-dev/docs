776be5a4891247d6d084023362316a999ad29e3e
hugodutka <hugodutka@users.noreply.github.com>
2020-10-26 18:53:06 +0000

    deploy: c0d7e459c50df2eb8e760beccaa862cc608828c4

--- install.sh
+++ install.sh
@@ -9,11 +9,7 @@ Check out https://hocus.dev/installation.html for more information.
 "
     POST_INSTALLATION_MSG="
 Hocus has been installed.
-To verify the installation was successful, restart your shell or run
-
-$ source ~/.profile
-
-and then run
+To verify the installation was successful, restart your shell and run
 
 $ hocus
 
@@ -21,11 +17,12 @@ If it was not, please ensure that $HOME/bin is in PATH and that $HOME/bin/hocus 
 "
 
     case "$(uname -a)" in
-        *"Darwin"* ) FILENAME="hocus-macos-x86_64";;
-        *"GNU/Linux"* ) FILENAME="hocus-linux-x86_64-gnu";;
+        *"Darwin"* ) FILENAME="hocus-macos-x86_64" && PLATFORM="macos";;
+        *"GNU/Linux"* ) FILENAME="hocus-linux-x86_64-gnu" && PLATFORM="linux";;
         * ) echo "$UNSUPPORTED_OS_MSG" && return 1;;
     esac
 
+    echo "$SHELL"
     echo "Downloading hocus..."
 
     mkdir -p "$HOME/bin"
@@ -35,7 +32,23 @@ If it was not, please ensure that $HOME/bin is in PATH and that $HOME/bin/hocus 
     chmod +x "$OUTPUT_LOCATION"
 
     if [ ":$PATH:" != *":$HOME/bin:"* ]; then
-        echo 'export PATH="$PATH:$HOME/bin"' >> "$HOME/.profile"
+        PATH_SNIPPET='export PATH="$PATH:$HOME/bin"'
+        
+        case "$SHELL" in
+            *"/bash"* ) SHELL_NAME="bash";;
+            *"/zsh"* ) SHELL_NAME="zsh";;
+            * ) SHELL_NAME="unknown" ;;
+        esac
+
+        if [ "$SHELL_NAME" = "bash" ] && [ $PLATFORM = "linux" ]; then
+            echo "$PATH_SNIPPET" >> "$HOME/.bashrc"
+        elif [ "$SHELL_NAME" = "bash" ] && [ $PLATFORM = "macos" ]; then
+            echo "$PATH_SNIPPET" >> "$HOME/.bash_profile"
+        elif [ "$SHELL_NAME" = "zsh" ]; then
+            echo "$PATH_SNIPPET" >> "$HOME/.zshrc"
+        else
+            echo "Unsupported default shell; please add '$HOME/bin' to PATH yourself."
+        fi
     fi
 
     echo "$POST_INSTALLATION_MSG"
